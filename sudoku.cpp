b77bcfef9227906dc07fa50c905f72d5d9e992c5
L33t Hax0r <lujordan124@gmail.com>
2015-09-28 23:42:29 -0400

    Finished sudoku validator

--- sudoku.cpp
+++ sudoku.cpp
@@ -16,39 +16,96 @@ struct cubeData {
 };
 
 int sudoku[9][9];
+int valid = 1;
 
 void *checkCube(void* args) {
 	struct cubeData *data = (struct cubeData*)args;
-	cout << data->col << " " << data->row << endl;
-	cout << "CHECKING CUBES" << endl;
 
 	int num[9];
 	int i;
 	for (i = 0; i < 9; i++) {
-		num[i] = i;
+		num[i] = 0;
+	}
+
+	int j;
+	for (i = data->row; i < data->row + 3; i++) {
+		for (j = data->col; j < data->col + 3; j++) {
+			num[sudoku[i][j]-1]++;
+		}
+	}
+
+	for (i = 0; i < 9; i++) {
+		if (num[i] == 0) {
+			cout << i+1 << " is missing from subsection: rows(" << data->row << "..." << data->row+3 << "), cols(" << data->col << "..." << data->col+3 << ")" << endl;
+			valid = 0;
+		}
 	}
 
 	return NULL;
 }
 
 void *checkAllRows(void* args) {
-	cout << "CHECKING ROWS" << endl;
 	int num[9];
-	int i;
-	for (i = 0; i < 9; i++) {
-		num[i] = i;
+	int count;
+	for (count = 0; count < 9; count++) {
+		num[count] = 0;
 	}
+
+	int i;
+	int j;
+	int k;
+	for (i = 0; i < 9; i++) {
+		for (j = 0; j < 9; j++) {
+			num[sudoku[i][j]-1]++;
+		}
+
+		for(k = 0; k < 9; k++) {
+			if (num[k] == 0) {
+				cout << k+1 << " is missing from row " << i + 1 << endl;
+				valid = 0;
+			}
+		}
+
+		for (count = 0; count < 9; count++) {
+			num[count] = 0;
+		}
+	}
+
 	return NULL;
 }
 
 void *checkAllColumns(void* args) {
-	cout << "CHECKING COLUMNS" << endl;
 	int num[9];
-	int i;
-	for (i = 0; i < 9; i++) {
-		num[i] = i;
+	int count;
+	for (count = 0; count < 9; count++) {
+		num[count] = 0;
 	}
+
+	int i;
+	int j;
+	int k;
+	for (i = 0; i < 9; i++) {
+		for (j = 0; j < 9; j++) {
+			num[sudoku[j][i]-1]++;
+		}
+
+		for(k = 0; k < 9; k++) {
+			if (num[k] == 0) {
+				cout << k+1 << " is missing from column " << i + 1 << endl;
+				valid = 0;
+			}
+		}
+
+		for (count = 0; count < 9; count++) {
+			num[count] = 0;
+		}
+	}
+
 	return NULL;
+}
+
+void checkValid(int nums[]) {
+
 }
 
 void parseLine(char *line) {
@@ -73,8 +130,8 @@ void parseLine(char *line) {
 		int rowThread = pthread_create(&threads[0], NULL, &checkAllRows, NULL);
 		int colThread = pthread_create(&threads[1], NULL, &checkAllColumns, NULL);
 
-		pthread_join(rowThread, NULL);
-		pthread_join(colThread, NULL);
+		pthread_join(threads[0], NULL);
+		pthread_join(threads[1], NULL);
 
 		int i = 0;
 		int j = 0;
@@ -86,9 +143,15 @@ void parseLine(char *line) {
 				readParams->col = j;
 
 				int cubeThreads = pthread_create(&threads[threadCount], NULL, &checkCube, readParams);
-				pthread_join(cubeThreads, NULL);
+				pthread_join(threads[threadCount], NULL);
 				threadCount++;
 			}
+		}
+
+		if (valid == 0) {
+			cout << "Sudoku is invalid" << endl;
+		} else {
+			cout << "Sudoku is valid" << endl;
 		}
 
 		pthread_exit(threads);
